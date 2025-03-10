98afc9423d311b84b574a7897ecedc89080ce998
Lyubomir Georgiev <lyubomirgeorgiev2712@gmail.com>
2025-03-10 17:15:50 +0200

    base project initialized with the navbar made and logo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from "./Components/Navbar/Navbar.jsx";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <Navbar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import './Navbar.css';
+import Logo from "./Logo.jsx";
+
+export default function Navbar() {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar className={"navbar"} position="fixed" sx={{left: '50%', transform: 'translateX(-50%)', width: '60%', backgroundColor: 'black', paddingLeft: '0'}}>
+                <Toolbar sx={{padding: 0}} >
+                    <Logo/>
+                    <h3>Lyubomir Georgiev</h3>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 2 }}>
+                    </Typography>
+                    <Button className={"navbar-item"} color="white" style={{fontSize: "1.5rem", textTransform: "none"}}>Intro</Button>
+                    <Button className={"navbar-item"} color="white" style={{fontSize: "1.5rem", textTransform: "none"}}>Projects</Button>
+                    <Button className={"navbar-item"} color="white" style={{fontSize: "1.5rem", textTransform: "none"}}>Contact Me</Button>
+                </Toolbar>
+                </AppBar>
+
+        </Box>
+    );
+}
