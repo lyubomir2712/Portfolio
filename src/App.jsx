2c38f506f29e9968a4d96e30e0ab9678038e4efb
Lyubomir Georgiev <lyubomirgeorgiev2712@gmail.com>
2025-03-11 16:57:11 +0200

    added the certificationSection component and some features for its layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Navbar from "./Components/Navbar/Navbar.jsx";
 import IntroductionSection from "./Components/IntroductionSection/IntroductionSection.jsx";
+import {CertificationSection} from "./Components/CertificationSection/CertificationSection.jsx";
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
     <>
         <Navbar/>
         <IntroductionSection/>
+        <CertificationSection/>
     </>
   )
 }
---
+++ src/Components/CertificationSection/CertificationSection.jsx
@@ -0,0 +1,15 @@
+import './CertificationSection.css'
+export function CertificationSection() {
+    return (
+        <section className="certification-section">
+            <article className="certificates-article">
+
+            </article>
+            <article className="certificates-description">
+                <h3>Software University</h3>
+                <h5>.NET Fullstack developer</h5>
+                <p>An instense software enineering program from which I acquired invaluable skills to create modern web applications.</p>
+            </article>
+        </section>
+    )
+}
