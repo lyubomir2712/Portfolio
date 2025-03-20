a6ade45eb2547fe4dc83c6ea572e02c8728e9de3
Lyubomir Georgiev <lyubomirgeorgiev2712@gmail.com>
2025-03-20 21:10:22 +0200

    installed framer-motion and adding certificates in the certification component

--- src/Components/CertificationSection/CertificationSection.jsx
+++ src/Components/CertificationSection/CertificationSection.jsx
@@ -3,6 +3,17 @@ export default function CertificationSection() {
     return (
         <section id="certification-section" className="certification-section">
             <article className="certificates-article">
+                <div className="certificates-container">
+                    <div className="certificate-one-container certificate-container">
+                        <img className="certificate-one certificate" src="../../../public/certificate.jpg" alt="Certificate One" />
+                    </div>
+                    <div className="certificate-two-container certificate-container">
+                        <img className="certificate-two certificate" src="../../../public/certificate.jpg" alt="Certificate Two" />
+                    </div>
+                    <div className="certificate-three-container certificate-container">
+                        <img className="certificate-three certificate" src="../../../public/certificate.jpg" alt="Certificate Three" />
+                    </div>
+                </div>
 
             </article>
             <article className="certificates-description">
