df385bcb31cec76e16badce7cc2d8d69a613df92
Lyubomir Georgiev <lyubomirgeorgiev2712@gmail.com>
2025-03-21 13:45:13 +0200

    aligned the certificates better

--- src/Components/CertificationSection/CertificationSection.jsx
+++ src/Components/CertificationSection/CertificationSection.jsx
@@ -1,20 +1,46 @@
 import './CertificationSection.css'
+import {motion} from "framer-motion";
 export default function CertificationSection() {
     return (
         <section id="certification-section" className="certification-section">
             <article className="certificates-article">
                 <div className="certificates-container">
-                    <div className="certificate-one-container certificate-container">
+                    <motion.div
+                        className="certificate-one-container certificate-container"
+                        animate={{ y: [0, -10, 0] }}
+                        transition={{
+                            duration: 3,
+                            repeat: Infinity,
+                            ease: "easeInOut",
+                        }}
+                    >
                         <img className="certificate-one certificate" src="../../../public/certificate.jpg" alt="Certificate One" />
-                    </div>
-                    <div className="certificate-two-container certificate-container">
-                        <img className="certificate-two certificate" src="../../../public/certificate.jpg" alt="Certificate Two" />
-                    </div>
-                    <div className="certificate-three-container certificate-container">
-                        <img className="certificate-three certificate" src="../../../public/certificate.jpg" alt="Certificate Three" />
-                    </div>
-                </div>
+                    </motion.div>
 
+                    <motion.div
+                        className="certificate-two-container certificate-container"
+                        animate={{ y: [0, -10, 0] }}
+                        transition={{
+                            duration: 3,
+                            repeat: Infinity,
+                            ease: "easeInOut",
+                        }}
+                    >
+                        <img className="certificate-two certificate" src="../../../public/certificate.jpg" alt="Certificate Two" />
+                    </motion.div>
+
+                    <motion.div
+                        className="certificate-three-container certificate-container"
+                        animate={{ y: [0, -10, 0] }}
+                        transition={{
+                            duration: 3,
+                            repeat: Infinity,
+                            ease: "easeInOut",
+                        }}
+                    >
+                        <img className="certificate-three certificate" src="../../../public/certificate.jpg" alt="Certificate Three" />
+                    </motion.div>
+                </div>
             </article>
             <article className="certificates-description">
                 <h3>Software University</h3>
