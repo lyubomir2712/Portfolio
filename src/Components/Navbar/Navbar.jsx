f1c8f92e0fc03d139d83bf77a7fc689292ff080c
Lyubomir Georgiev <lyubomirgeorgiev2712@gmail.com>
2025-03-22 17:31:18 +0200

    added rotation animation to the logo on hover

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,16 +4,15 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import './Navbar.css';
-import Logo from "./Logo.jsx";
 
 export default function Navbar() {
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar className={"navbar"} position="fixed" sx={{left: '50%', transform: 'translateX(-50%)', width: '51%', backgroundColor: '#100d1ead', paddingLeft: '0'}}>
                 <Toolbar sx={{padding: 0}}>
-                    {/*<p><a href="#"><Logo/></a></p>*/}
-                    <i className="bi bi-code-slash logo"></i>
+                    <div className="logo-name-container"><i className="bi bi-code-slash logo"></i>
                     <h3 className={"navbar-name"}>Lyubomir Georgiev</h3>
+                    </div>
                     <Typography variant="h6" component="div" sx={{flexGrow: 2}}>
                     </Typography>
                     <a className="nav-link" href="#introduction-section"><Button className={"navbar-item"} color="white"
