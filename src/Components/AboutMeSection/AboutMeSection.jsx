1b9d73b813ecebb59a2b8f84771ad44303105623
Lyubomir Georgiev <lyubomirgeorgiev2712@gmail.com>
2025-06-25 16:14:49 +0300

    made small changes to introduction and about me section

--- src/Components/AboutMeSection/AboutMeSection.jsx
+++ src/Components/AboutMeSection/AboutMeSection.jsx
@@ -6,13 +6,13 @@ export default function AboutMeSection() {
             <h1 className="about-me-header text-2xl mb-4 text-[#16F2B3] text-left">WHO I AM?</h1>
             <div className="about-me-container grid grid-cols-1 md:grid-cols-2 gap-4 ">
 
-                <div className="about-me-container text-left ">
+                <div className="about-me-container text-left text-lg">
                     I am a 21-year-old junior fullstack engineer with expertise in React and .NET, currently in my third year of a Software Engineering degree at the University of Veliko Tarnovo. With two years of experience as a system administrator, I have transitioned over the past few months into software engineering, where I work as a junior fullstack engineer. I am highly proficient in version control systems, Agile methodologies, and CI/CD pipelines. My experience extends to real-world projects, where I’ve developed a strong understanding of software craftsmanship. Additionally, I possess solid knowledge in object-oriented programming (OOP), design patterns, as well as data structures and algorithms. I am always eager to embrace new challenges and opportunities that allow me to grow and contribute to innovative projects.
                 </div>
 
                 <div className="about-me-cell2 about-me-profile grid [grid-template-columns:3fr_1fr]   justify-items-end">
                     <img
-                        className="profile-img w-63 h-auto rounded-lg grayscale hover:grayscale-0 hover:scale-105 transition duration-700 ease-in-out"
+                        className="profile-img w-75 h-auto rounded-lg grayscale hover:grayscale-0 hover:scale-105 transition duration-700 ease-in-out"
                         src="src/assets/profile.jpeg" alt="profile"/>
 
                     <span
