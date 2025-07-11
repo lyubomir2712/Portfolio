fee9d2abb75756ea76f419b5c11fbe08be22b2c7
Lyubomir Georgiev <lyubomirgeorgiev2712@gmail.com>
2025-07-11 23:25:02 +0300

    added react observer to education section

--- src/Components/EducationSection/EducationSection.jsx
+++ src/Components/EducationSection/EducationSection.jsx
@@ -1,3 +1,4 @@
+import { useInView } from 'react-intersection-observer';
 import { motion } from "framer-motion";
 import "./EducationSection.css"
 import Card from "../Card/Card.jsx";
@@ -6,8 +7,12 @@ import Title from "../Title/Title.jsx";
 import DivisionLine from "../DivisionLine/DivisionLine.jsx";
 import EducationBackgroundSvg from "./EducationBackgroundSvg.jsx";
 export default function EducationSection() {
+    const { ref, inView } = useInView({
+        triggerOnce: true,
+        threshold: 0.1,
+    });
     return (
-        <section className="education-section">
+        <section className="education-section" ref={ref}>
             <DivisionLine />
             <br/> <br/>
 
@@ -18,16 +23,14 @@ export default function EducationSection() {
             <div className="education-main">
                 <motion.div
                     className="education-cell-one"
-                    initial={{ opacity: 0, x: -30 }}
-                    animate={{ opacity: 1, x: 0 }}
+                    animate={inView ? { opacity: 1, x: 0 } : { opacity: 0, x: -30 }}
                     transition={{ duration: 1.75 }}
                 >
                     <SvgEducation/>
                 </motion.div>
                 <motion.div
                     className="education-cell-two"
-                    initial={{ opacity: 0, x: 60 }}
-                    animate={{ opacity: 1, x: 0 }}
+                    animate={inView ? { opacity: 1, x: 0 } : { opacity: 0, x: 60 }}
                     transition={{ duration: 1, delay: 0.2 }}
                 >
                     <Card date={"09/2022 - 01/2026"} title={"Software Engineering/Bachelor"} text={"University of Veliko Tarnovo"}/>
