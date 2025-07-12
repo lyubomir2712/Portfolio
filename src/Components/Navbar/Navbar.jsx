5ea7da8ab0e021bbb83ace1a4dc71a5b2cf84dd5
Lyubomir Georgiev <lyubomirgeorgiev2712@gmail.com>
2025-07-13 00:24:06 +0300

    refactored the navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -9,42 +9,53 @@ export default function Navbar() {
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar className={"navbar"} position="sticky" sx={{ width: '100%', backgroundColor: 'transparent', paddingLeft: '0', marginTop: '0', border: "none", boxShadow: 'none'}}>
+
                 <Toolbar sx={{padding: 0}}>
-                    <div className="logo-name-container"><i className="bi bi-code-slash logo"></i>
-                    <span className={"navbar-name"}>Lyubomir Georgiev</span>
+
+                    <div className="logo-name-container">
+                        <i className="bi bi-code-slash logo"></i>
+
+                        <span className={"navbar-name"}>Lyubomir Georgiev</span>
                     </div>
-                    <Typography variant="h6" component="div" sx={{flexGrow: 2}}>
-                    </Typography>
+
+                    <Typography variant="h6" component="div" sx={{flexGrow: 2}}></Typography>
+
                     <a className="nav-link" href="#introduction-section"><Button className={"navbar-item"} color="white"
                                                                                  style={{
                                                                                      fontSize: "1.1rem",
                                                                                      textTransform: "none"
                                                                                  }}>Intro</Button></a>
+
                     <a className="nav-link" href="#projects-section"><Button className={"navbar-item"} color="white"
                                                                              style={{
                                                                                  fontSize: "1.1rem",
                                                                                  textTransform: "none"
                                                                              }}>Projects</Button></a>
+
                     <a className="nav-link" href="#certification-section"><Button className={"navbar-item"} color="white"
                                                                              style={{
                                                                                  fontSize: "1.1rem",
                                                                                  textTransform: "none"
                                                                              }}>Certifications</Button></a>
+
                     <a className="nav-link" href="#projects-section"><Button className={"navbar-item"} color="white"
                                                                                   style={{
                                                                                       fontSize: "1.1rem",
                                                                                       textTransform: "none"
                                                                                   }}>Projects</Button></a>
+
                     <a className="nav-link" href="#experience-section"><Button className={"navbar-item"} color="white"
                                                                              style={{
                                                                                  fontSize: "1.1rem",
                                                                                  textTransform: "none"
                                                                              }}>Experience</Button></a>
+
                     <a className="nav-link" href="#skills-section"><Button className={"navbar-item"} color="white"
                                                                                style={{
                                                                                    fontSize: "1.1rem",
                                                                                    textTransform: "none"
                                                                                }}>Skills</Button></a>
+
                     <a className="nav-link" href="#contact-section"><Button className={"navbar-item"} color="white"
                                                                             style={{
                                                                                 fontSize: "1.1rem",
