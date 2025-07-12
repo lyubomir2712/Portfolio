9abe94ecbde5b953be69e1e573a8be06cc61b885
Lyubomir Georgiev <lyubomirgeorgiev2712@gmail.com>
2025-07-12 23:47:45 +0300

    refactored the card component

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -9,6 +9,7 @@ export default function Card({date, title, text}) {
                 <g opacity="0.22" filter="url(#filter0_f_94_12716)">
                     <circle cx="585" cy="473" r="329" fill="#5B21B6"/>
                 </g>
+
                 <defs>
                     <filter id="filter0_f_94_12716" x="-18" y="-130" width="1206" height="1206" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
                         <feFlood floodOpacity="0" result="BackgroundImageFix"/>
@@ -20,7 +21,6 @@ export default function Card({date, title, text}) {
 
             <div className="card-inner">
                 <div className="card-dates card-cell1">{date}</div>
-
                 <div className="card-icon card-cell2"><CardIcon/></div>
                 <div className="card-title card-cell3">{title}</div>
                 <div className="card-text card-cell4">{text}</div>
