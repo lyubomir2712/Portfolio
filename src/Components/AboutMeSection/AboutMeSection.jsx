06b30b3963a679d9437896757bbf5de080ee6950
Lyubomir Georgiev <lyubomirgeorgiev2712@gmail.com>
2025-07-15 13:53:13 +0300

    made the hover animation faster

--- src/Components/AboutMeSection/AboutMeSection.jsx
+++ src/Components/AboutMeSection/AboutMeSection.jsx
@@ -34,7 +34,7 @@ export default function AboutMeSection() {
                         initial={{ x: 30, opacity: 0, filter: "grayscale(100%)", scale: 1 }}
                         animate={inView ? { x: 0, opacity: 1 } : { x: 30, opacity: 0 }}
                         whileHover={{ filter: "grayscale(0%)", scale: 1.05 }}
-                        transition={{ duration: 1.75 }}/>
+                        transition={{ duration: 0.5 }}/>
 
                     <motion.span
                         className="bg-[#1a1443] w-38 h-10 inline-block text-white rotate-90 mt-14 p-2 px-3 text-xl rounded-md about-me-title"
