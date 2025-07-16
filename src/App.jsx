ad3e07969ce240e71229d020583ffb9b6f8b7064
Lyubomir Georgiev <lyubomirgeorgiev2712@gmail.com>
2025-07-16 17:31:04 +0300

    added custom cursor

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,25 @@ import EducationSection from "./Components/EducationSection/EducationSection.jsx
 import ExperienceSection from "./Components/ExperienceSection/ExperienceSection.jsx";
 import AboutMeSection from "./Components/AboutMeSection/AboutMeSection.jsx";
 import ScrollTopButton from "./Components/ScrollTopButton/ScrollTopButton.jsx";
+import handPointer from './assets/Icons/hand-pointer.png';
+import {useEffect} from "react";
+
 
 function App() {
+    useEffect(() => {
+        const pointerStyle = `url(${handPointer}) 0 0, pointer`;
+        const clickableElements = document.querySelectorAll('button, a, [role="button"]');
 
+        clickableElements.forEach(el => {
+            el.style.cursor = pointerStyle;
+        });
+
+        return () => {
+            clickableElements.forEach(el => {
+                el.style.cursor = ''; // Reset on cleanup
+            });
+        };
+    }, [handPointer]);
   return (
     <>
         <Navbar/>
